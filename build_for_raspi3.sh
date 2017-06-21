42464f23f63a5bc04a4b29c35f298514b793d599
Jose Leon <leonj1@gmail.com>
2017-06-21 00:54:28 -0400

    Dockerizing for resin.io

--- build_for_raspi3.sh
+++ build_for_raspi3.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=arm GOARM=7 go build -v -o speedtest-cli-raspi3 speedtest.go
-scp speedtest-cli-raspi3 pi@10.0.0.38:/tmp
+if [ -d target ]; then
+   rm -rf target
+fi
+mkdir -p target
+
+GOOS=linux GOARCH=arm GOARM=7 go build -v -o target/speedtest-cli-raspi3 speedtest.go
+#scp speedtest-cli-raspi3 pi@10.0.0.38:/tmp
 
